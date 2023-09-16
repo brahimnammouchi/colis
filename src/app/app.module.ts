12978a8eb4d8f1b5850cef7b3b838ab4c7e8a76f
brahimnammouchi <brahim.namouchi@gmail.com>
2023-09-16 13:17:04 +0100

    sans firebase en marche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,6 @@ import { ContactComponent } from './contact/contact.component';
 import { PricingComponent } from './pricing/pricing.component';
 import { ServicesComponent } from './services/services.component';
 import { TrackingComponent } from './tracking/tracking.component';
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,16 +15,10 @@ import { LoginComponent } from './login/login.component';
     PricingComponent,
     ServicesComponent,
     TrackingComponent,
-    RegisterComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFirestoreModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
